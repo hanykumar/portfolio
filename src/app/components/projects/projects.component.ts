02a9b518ee80b7e8f08b9d96c0fe8c117a1cd760
hany kumar <email@example.com>
2023-12-24 00:25:26 +0530

    order chnage image

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,19 +13,19 @@ export class ProjectsComponent {
   tasklist_app_images = [
     'assets/tasklist-app-images/img_1.jpg',
     'assets/tasklist-app-images/img_2.jpg',
-    'assets/tasklist-app-images/img_3.jpg',
-    'assets/tasklist-app-images/img_7.jpg',
     'assets/tasklist-app-images/img_4.jpg',
     'assets/tasklist-app-images/img_5.jpg',
     'assets/tasklist-app-images/img_6.jpg',
+    'assets/tasklist-app-images/img_7.jpg',
+    'assets/tasklist-app-images/img_3.jpg',
   ]
 
   tasklist_web_images = [
     'assets/tasklist-web-images/img_web_1.png',
     'assets/tasklist-web-images/img_web_2.png',
-    'assets/tasklist-web-images/img_web_3.png',
     'assets/tasklist-web-images/img_web_4.png',
     'assets/tasklist-web-images/img_web_5.png',
     'assets/tasklist-web-images/img_web_6.png',
+    'assets/tasklist-web-images/img_web_3.png',
   ]
 }
