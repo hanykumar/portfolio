3e7bef794b5350f3e656ddd5198a727e0b27690f
hany kumar <email@example.com>
2024-01-25 15:15:12 +0530

    experience updated

--- src/assets/experience.js
+++ src/assets/experience.js
@@ -6,10 +6,11 @@ export const Experience = [
       "startDate": "Aug 2021",
       "endDate": "Oct 2023",
       "responsibilities": [
+        "Worked on two client projects of inventory management systems",
         "Complete website transformation for the client, ensuring the requirements, design, responsiveness, restructuring of code, and lazy loading resulted into much faster loading website.",
-        "Coordinated the dynamic presentation of data in nested tables, including images, implementing innovative approaches for dynamic reports, which resulted in a unified report for multiple types of reports with unified table and image modules.",
-        "Implemented the drawing/plotting of contours over images, dynamically changing with a slider for the best user experience, as per the requirement. ",
-        "Development of Java-based APIs for the backend infrastructure, involving coding and maintenance of RESTful APIs to ensure sustained seamless data transmission and integration of functionalities. "
+        "Coordinated the dynamic presentation of data in nested tables, including images, implementing innovative approaches for dynamic reports, which resulted in a unified report for multiple types of reports with unified table and image modules by maintaining states using REDUX.",
+        "Implemented the drawing/plotting of contours over images, dynamically changing with a slider for the best user experience, as per the requirement.",
+        "Development of restful APIs for the backend infrastructure, involving coding and maintenance of RESTful APIs to ensure sustained seamless data transmission and integration of functionalities."
       ]
     },
     {
@@ -19,10 +20,10 @@ export const Experience = [
       "startDate": "Jan 2020",
       "endDate": "Jul 2021",
       "responsibilities": [
-        "Played a key role in enhancing live websites such as GeekyAnts, TheTopGeek, and NativeBase.",
-        "Spearheaded web development for TheTopGeek, a dynamic job portal, ensuring impactful contributions",
-        "Contributed significantly to the advancement of NativeBase, a React-Native mobile app component library.",
-        "Drove backend development for the GeekyAnts website, optimizing performance and functionality."
+        "Worked on three projects such as GeekyAnts, TheTopGeek, and NativeBase.",
+        "Impactful contribution and improved implementation for GeekyAnts website and TheTopGeek website a dynamic job portal, ensuring impactful contributions by maintaining states using REDUX  ensuring the requirements, design, responsiveness, restructuring of code, and lazy loading.",
+        "Contributed significantly to the advancement of NativeBase library components, a React-Native mobile app component library.",
+        "Drove backend development for the GeekyAnts website, optimized performance and functionality also."
       ]
     },
     {
@@ -32,7 +33,7 @@ export const Experience = [
       "startDate": "Jul 2019",
       "endDate": "Dec 2019",
       "responsibilities": [
-        "Started career in learning and implementing web development to transform a desktop application into a web application, leveraging REST API services."
+        "Started career in learning and implementing web development to transform a desktop application into a web application, using REST API services."
       ]
     }
   ]
