c0a05aca1ea1e48abc0ead010962046c8d3d41ce
hany kumar <kumar.hany1994@gmail.com>
2024-01-28 14:12:29 +0530

    custom image slider

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgImageSliderModule } from 'ng-image-slider';
+import { ImageSliderComponent } from '../image-slider/image-slider.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule, NgImageSliderModule],
+  imports: [CommonModule, ImageSliderComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
@@ -19,12 +19,7 @@ export class ProjectsComponent {
     'assets/tasklist-app-images/img_6.jpg',
     'assets/tasklist-app-images/img_7.jpg',
     'assets/tasklist-app-images/img_3.jpg',
-  ].map((image: any, index: any) => ({
-    image: image,
-    thumbImage: image, // Assuming thumbnail path is the same as the image path in this example
-    alt: `Image ${index + 1}`,
-    order: index + 1,
-  }));
+  ]
 
 
   tasklist_web_images = [
@@ -34,32 +29,11 @@ export class ProjectsComponent {
     'assets/tasklist-web-images/img_web_5.png',
     'assets/tasklist-web-images/img_web_6.png',
     'assets/tasklist-web-images/img_web_3.png',
-  ].map((image: any, index: any) => ({
-    image: image,
-    thumbImage: image, // Assuming thumbnail path is the same as the image path in this example
-    alt: `Image ${index + 1}`,
-    order: index + 1,
-  }));
+  ]
 
 
   javascript_compiler_image = [
     'assets/javascriptcompiler.png',
-  ].map((image: any, index: any) => ({
-    image: image,
-    thumbImage: image, // Assuming thumbnail path is the same as the image path in this example
-    alt: `Image ${index + 1}`,
-    // title: `Image ${index + 1}`,
-    order: index + 1,
-  }));
+  ]
 
-
-  updateImageUrls = (imagesArray: any) => {
-    return imagesArray.map((image: any, index: any) => ({
-      image: image,
-      thumbImage: image, // Assuming thumbnail path is the same as the image path in this example
-      alt: `Image ${index + 1}`,
-      title: `Image ${index + 1}`,
-      order: index + 1,
-    }));
-  }
 }
