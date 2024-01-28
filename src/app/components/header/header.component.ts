acd9c163713d7252f43ffa454350dc28293fdc58
hany kumar <kumar.hany1994@gmail.com>
2024-01-28 18:39:37 +0530

    hover menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+// @ts-ignore
+import { HeaderMenu } from '../../../assets/header.js';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   profile_pic: string = 'assets/profile_pic.jpg'
   linkedin_icon = 'assets/icons/linkedin.svg'
+  headerMenu = HeaderMenu;
+  isNavbarCollapsed = true;
 
-   closeNavbar = () => {
+  closeNavbar = () => {
     var navbarToggleBtn = document.getElementById('navbarToggleBtn');
-    if (navbarToggleBtn) {
-        navbarToggleBtn.click();
+    if (navbarToggleBtn && !this.isNavbarCollapsed) {
+      navbarToggleBtn.click();
     }
-}
+  }
+
+  toggleNavbar(): void {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
---
+++ src/assets/header.js
@@ -0,0 +1,28 @@
+export const HeaderMenu = [{
+    href: '#projects',
+    title: 'Projects'
+},
+{
+    href: '#experience',
+    title: 'Experience'
+},
+{
+    href: '#technical-skills',
+    title: 'Technical Skills'
+},
+{
+    href: '#education',
+    title: 'Education'
+},
+{
+    href: '#publication',
+    title: 'Publication'
+},
+{
+    href: '#about',
+    title: 'About Me'
+},
+{
+    href: '#resume',
+    title: 'Resume'
+}]
